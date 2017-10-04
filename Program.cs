8bdc6c5e6da349b12cd97f971cb2b7ba55af09ca
Rassul Yunussov <yunussov@gmail.com>
2017-10-04 12:09:06 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace ms
+{
+    class Program
+    {
+        static void MergeSort(int [] src)
+        {
+            int [] buff = new int [src.Length];
+            ms(0,src.Length);
+            void ms(int left,int right)
+            {
+                int length = right-left;
+                int mid = (length)/2;
+                int leftSide = left+mid;
+                int rightSide = length-mid;
+                if(mid>1)
+                    ms(left,leftSide);
+                if(rightSide>1)
+                    ms(leftSide,right);
+                int i =0,j=0,k=0;
+                while(i<mid&&j<rightSide)
+                {
+                    if(src[left+i]<src[leftSide+j])
+                        buff[k++]=src[left+i++];
+                    else
+                        buff[k++]=src[leftSide+j++];
+                }
+                while(i<mid)
+                    buff[k++]=src[left+i++];
+                while(j<rightSide)
+                    buff[k++]=src[leftSide+j++];
+                Buffer.BlockCopy(buff,0,src,left*sizeof(int),(length)*sizeof(int));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int [] a = Enumerable.Range(0,1000)
+                                .Select(i=>r.Next(1,1000))
+                                .ToArray();
+            MergeSort(a);
+        }
+    }
+}
